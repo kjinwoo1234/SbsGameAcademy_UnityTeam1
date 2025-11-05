9acaf010ad775614d7fda9e842ba3b7d74399b6b
rudwn9641-sudo <rudwn9641@gmail.com>
2025-11-06 01:40:11 +0900

    feat: SceneBase.cs 추가 (공통 베이스 클래스)
    공통 Scene 베이스(템플릿 메서드)

---
+++ MUDGAME/MUDGAME/SceneBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class1
+{
+    public interface IObject
+    {
+        void OnStart();
+        void OnKeyInput(ConsoleKey key);
+        void Update();
+        void Render();
+    }
+
+    public class Scene : IObject
+    {
+        protected List<IObject> childObject = new List<IObject>();
+        private Scene _nextScene = null;
+        public Scene nextScene => _nextScene;
+
+        public virtual void OnStart()
+        {
+            foreach (IObject child in childObject) child.OnStart();
+        }
+        public virtual void OnKeyInput(ConsoleKey key)
+        {
+            foreach (IObject child in childObject) child.OnKeyInput(key);
+        }
+        public virtual void Update()
+        {
+            foreach (IObject child in childObject) child.Update();
+        }
+        public virtual void Render()
+        {
+            foreach (IObject child in childObject) child.Render();
+        }
+
+        protected void ChangeScene(Scene newScene)
+        {
+            _nextScene = newScene;
+        }
+    }
+}
